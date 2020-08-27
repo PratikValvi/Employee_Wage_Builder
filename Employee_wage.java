35e44e1b26fa2b3b103207d605bed7bd171c98cc
Pratik Valvi <pratik@pop-os.localdomain>
2020-08-27 14:58:24 +0530

    Employee Wage Builder in Progress

---
+++ Employee_wage.java
@@ -0,0 +1,17 @@
+/* 
+Program for Computing Employee Wages
+*
+*
+version 1.0
+*
+*
+author:Pratik R Valvi
+*/
+
+class Employee_wage {
+	
+	public static void main (String args[]) {
+		
+		System.out.println("**********Welcome to Employee Wage Computation**********");
+	}
+}
