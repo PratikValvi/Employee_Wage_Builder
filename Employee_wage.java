b4dd9dbb2959224662a04c833bba88e63db53f74
Pratik Valvi <pratik@pop-os.localdomain>
2020-08-27 15:20:30 +0530

    Feature: Employee Daily Wage Calculator

--- Employee_wage.java
+++ Employee_wage.java
@@ -9,18 +9,34 @@ author:Pratik R Valvi
 */
 
 class Employee_wage {
-	
-	public static void main (String args[]) {
-		//Variables
-		int emp_check = (int) (Math.random()*10)%2;
+	//Constants
+	public static final int emp_rate_per_hour = 100;
+	public static final int emp_full_time = 0;
+	public static final int emp_part_time = 1;
 		
+	public static void main (String args[]) {
 		System.out.println("**********Welcome to Employee Wage Computation**********");
 		
-		//Feature Check Employee Attendeance
-		if (emp_check==0) {
-			System.out.println("Employee is Absent");
-		} else {
-			System.out.println("Employee is Present");
+		//Variables
+		int emp_wage;
+		int emp_hours;
+		
+		//Feature : Check Employee Attendance
+		int emp_check = (int) (Math.random()*10)%3;
+		switch(emp_check) {
+			case emp_full_time:
+				emp_hours = 8; //Employee Hours for Full Time
+				break;
+			case emp_part_time:
+				emp_hours = 4; //Employee Hours for Part Time
+				break;
+			default:
+				emp_hours = 0; //Employee Absent
+				break;
 		}
+		
+		//Feature : Compute Employee Daily Wage based on Attendance
+		emp_wage = emp_hours*emp_rate_per_hour;
+		System.out.println("Employee Daily Wage: "+emp_wage);
 	}
 }
