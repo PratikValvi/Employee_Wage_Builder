7b2dd6b64d3176b48cde43783e768c7704daea06
Pratik Valvi <pratik@pop-os.localdomain>
2020-08-27 15:06:09 +0530

    Feature: Check Employee Attendance

--- Employee_wage.java
+++ Employee_wage.java
@@ -11,7 +11,16 @@ author:Pratik R Valvi
 class Employee_wage {
 	
 	public static void main (String args[]) {
+		//Variables
+		int emp_check = (int) (Math.random()*10)%2;
 		
 		System.out.println("**********Welcome to Employee Wage Computation**********");
+		
+		//Feature Check Employee Attendeance
+		if (emp_check==0) {
+			System.out.println("Employee is Absent");
+		} else {
+			System.out.println("Employee is Present");
+		}
 	}
 }
