530a2ca914603ae9029e1396404b719e73e024a6
Pratik Valvi <pratik@pop-os.localdomain>
2020-08-29 12:00:31 +0530

    Employee Wage Computed & Stored in Array

--- Employee_wage.java
+++ Employee_wage.java
@@ -12,37 +12,47 @@ class Employee_wage {
 	//Constants
 	public static final int emp_full_time = 0;
 	public static final int emp_part_time = 1;
+
+	private int no_of_company = 0;
 	
-	//Private Variables
-	private final String company_name;
-	private final int max_working_days;
-	private final int max_working_hours;
-	private final int emp_rate_per_hour;
-	private int emp_total_wage;
-	private int emp_hours_count;
+	private Company_Employee_wage[] Company_Employee_Wage_Array;
 	
 	//Constructor
-	public Employee_wage(String company_name, int max_working_days, int max_working_hours, int emp_rate_per_hour) {
-		this.company_name = company_name;
-		this.max_working_days = max_working_days;
-		this.max_working_hours = max_working_hours;
-		this.emp_rate_per_hour = emp_rate_per_hour;
+	public Employee_wage() {
+		Company_Employee_Wage_Array = new Company_Employee_wage[2];
 	}
 	
-	//Function to Compute Employee Wage for Multiple Companies
-	public void computeWage() {
+	//Function to Add Company Information in Array
+	private void addCompany(String company_name, int max_working_days, int max_working_hours, int emp_rate_per_hour) {
+		Company_Employee_Wage_Array[no_of_company] = new Company_Employee_wage(company_name, max_working_days, max_working_hours, emp_rate_per_hour);
+		no_of_company++;
+	}
+	
+	//Function to Compute Company Wage for each element in Array
+	private void computeWage() {
+		for(int i=0;i<no_of_company;i++) {
+			Company_Employee_Wage_Array[i].setEmpTotalWage(this.computeWage(Company_Employee_Wage_Array[i]));
+			System.out.println(Company_Employee_Wage_Array[i]);
+		}
+	}
+	
+	//Function to Compute Wage
+	public int computeWage(Company_Employee_wage Company_Employee_wage) {
 		
 		//Variables
 		int emp_wage=0;
 		int emp_hours=0;
+		int emp_hours_count=0;
+		int total_wage=0;
+		
 		System.out.println();
-		System.out.println("For Company: "+company_name);
+		System.out.println("For Company: "+Company_Employee_wage.company_name);
 		
 		//Feature : Compute Employee Total Wage for Month
-		for (int i=1;i<=max_working_days;i++) {
+		for (int i=1;i<=Company_Employee_wage.max_working_days;i++) {
 			
 			//Feature : Check Employee Working Hours reached Maximum
-			if (emp_hours_count>=max_working_hours) {
+			if (emp_hours_count>=Company_Employee_wage.max_working_hours) {
 				break;
 			}
 			
@@ -63,27 +73,18 @@ class Employee_wage {
 			}
 			
 			//Feature : Compute Employee Daily Wage based on Attendance
-			emp_wage = emp_hours*emp_rate_per_hour;
-			emp_total_wage += emp_wage;
+			emp_wage = emp_hours*Company_Employee_wage.emp_rate_per_hour;
+			total_wage += emp_wage;
 			System.out.println("Employee Wage Day"+i+": "+emp_wage);
-		}	
+		}
+		return total_wage;
 	}
 	
-	public String toString() {
-		return "Employee Total Wage for "+company_name+" is: "+emp_total_wage;
-	}
-		
-	public static void main (String args[]) {
+	public static void main(String args[]) {
 		System.out.println("**********Welcome to Employee Wage Computation**********");
-		Employee_wage Dmart = new Employee_wage("Dmart",20,100,100);
-		Employee_wage Ola = new Employee_wage("Ola",25,150,120);
-		
-		//Computing Wage for Dmart
-		Dmart.computeWage();
-		System.out.println(Dmart);
-		
-		//Computing Wage for Ola
-		Ola.computeWage();
-		System.out.println(Ola);
+		Employee_wage empWageCalculator = new Employee_wage();
+		empWageCalculator.addCompany("Dmart",20,100,100);
+		empWageCalculator.addCompany("Ola",25,150,120);
+		empWageCalculator.computeWage();
 	}
 }
